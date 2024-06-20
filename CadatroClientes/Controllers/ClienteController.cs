67e800b1f030115d01ae791e2ebc4bdd7c3fdd99
raphaelaluvi <raphaluvi@gmail.com>
2024-06-20 08:24:15 -0300

    primeiro envioo

---
+++ CadatroClientes/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadatroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
